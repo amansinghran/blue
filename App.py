0a1a14f20fc4a476dfb0880710030f12df2b68cd
amansinghran <aman.singh@randstadusa.com>
2026-07-29 11:06:44 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from datetime import datetime
 import importlib
-from CreateUpload import generate_tei_output
+from CSFCreateUpload import CSFgenerate_tei_output
 
 st.set_page_config(page_title="r-Blue", layout="wide")
 st.markdown("<h1 style='text-align:center;'> TEI Upload </h1>", unsafe_allow_html=True)
@@ -10,8 +10,24 @@ left_co, cent_co, right_co = st.columns([1, 2, 1])
 
 with cent_co:
 
-    customer_options = ["IAP", "CustomerAA", "CustomerBB"] # Add all your customer names here
-    selected_customer = st.selectbox("Select Customer", options=customer_options)
+    customer_options = [
+        "ADL - Adler Pelzer",
+        "ARN - Ariens Company",
+        "BTD - Barrett Distribution",
+        "DHL - DHL_Spherion",
+        "IAP - International Auto Processing, Inc",
+        "MEI - MEI Corporation",
+        "NSY - Nasoya Foods USA LLC",
+        "PPI - Papa",
+        "SCK - Saddle Creek",
+        "SAS - Samsung",
+        "SCS - Schutz Container Systems, Inc",
+        "SWB - Smith & Wesson Brands, Inc",
+        "YKK - YKK USA, Inc",
+        ]
+
+    full_selection = st.selectbox("Select Customer", options=customer_options)
+    selected_customer = full_selection[:3].upper()
 
     source_file = st.file_uploader("Source File", type=["xls", "xlsx", "csv"])
     query_file = st.file_uploader("Query File", type=["xls", "xlsx", "csv"])
@@ -30,20 +46,23 @@ with cent_co:
         
         try:
             customer_module = importlib.import_module(f"Customers.{selected_customer}")
-            CustomerClass = getattr(customer_module, selected_customer)
+            customer_parse_func = getattr(customer_module, selected_customer)
         except ModuleNotFoundError:
             st.error(f"Could not find the module for {selected_customer}. Check your file structure.")
             st.stop()
+        except AttributeError:
+            st.error(f"Module {selected_customer} found, but function '{selected_customer}' is missing inside it.")
+            st.stop()
 
-        df_tei, df_err, custname = generate_tei_output(CustomerClass(source_file), query_file)
+        df_tei, df_err = CSFgenerate_tei_output(customer_parse_func(source_file), query_file)
         ts = datetime.now().strftime("%m%d%y_%H%M%S")
         
         st.session_state.out = {
             "tei": df_tei.to_csv(index=False).encode('utf-8'),
             "err": df_err.to_csv(index=False).encode('utf-8') if not df_err.empty else None,
-            "names": [f"TC_{custname}_{ts}.csv", f"TC_{custname}_{ts}_Error.csv"]
+            "names": [f"TC_{selected_customer}_{ts}.csv", f"TC_{selected_customer}_{ts}_Error.csv"]
         }
-        st.success(f"Files prepared for {custname}!")
+        st.success(f"Files available to download for {full_selection}!")
 
     if st.session_state.out:
         col1, col2 = st.columns(2)
